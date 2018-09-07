0c9bffc2ad5be07c55af4537db3c884662caf518
Hugo Trivino <kuerina@github.com>
2018-09-07 02:50:48 -0500

    working in the receiver

--- raspberry-pi/NRF24L01/receiver.py
+++ raspberry-pi/NRF24L01/receiver.py
@@ -4,37 +4,36 @@ import time
 import spidev
 
 GPIO.setmode(GPIO.BCM)
-pipes = [0xe6,0xe6,0xe6,0xe6,0xe6,0xe6]
+pipes=[[0x37,0x
 
 radio = NRF24(GPIO, spidev.SpiDev())
-radio.begin(0,17)
+radio.begin(0,6)
 radio.setPayloadSize(32)
-radio.setChannel(0x60)
-radio.setDataRate(NRF24.BR_2MBPS)
+radio.setChannel(0x72)
+radio.setDataRate(NRF24.BR_1MBPS)
 radio.setPALevel(NRF24.PA_MIN)
 radio.setAutoAck(True)
 radio.enableDynamicPayloads()
 radio.enableAckPayload()
-
 radio.openReadingPipe(1,pipes)
 radio.printDetails()
 
 radio.startListening()
 t=True
-while t:
-	ackPL=[1]
-	while not radio.available(0):
-		time.sleep(1/100.)
-	receivedMessage = []
-	radio.read(receivedMessage,radio.getDynamicPayloadSize())
-	print("Received: {}".format(receivedMessage))
-	print("Translating into unicode ...")
-	string = ""
-	for n in receivedMessage:
-		# decode
-		if (n>=32 and n <= 126):
-			string += chr(n)
-	print(string)
-	radio.writeAckPayload(1,ackPL,len(ackPL))
-	print("Loaded payload reply of {}".format(ackPL))
-	t=False
+while True:
+   ackPL=[1]
+   while not radio.available(0):
+      time.sleep(1/100.)
+   receivedMessage = []
+   radio.read(receivedMessage,radio.getDynamicPayloadSize())
+   print("Received: {}".format(receivedMessage))
+   print("Translating into unicode ...")
+   string = ""
+   for n in receivedMessage:
+   # decode
+      if ( n >= 32 and n <= 126):
+         string += chr(n)
+   print(string)
+   radio.writeAckPayload(1,ackPL,len(ackPL))
+   print("Loaded payload reply of {}".format(ackPL))
+   t=False
